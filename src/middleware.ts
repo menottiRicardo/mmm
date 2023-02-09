72a75bdf9107782edae707cdfaf326ebe86bef8b
Ricardo Menotti <ricardomenotti31@gmail.com>
2023-02-08 20:50:50 -0500

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getServerAuthSession } from "./server/auth";
-import { getServerSession } from "next-auth";
 import { getSession } from "next-auth/react";
 
 // This function can be marked `async` if using `await` inside
@@ -11,7 +9,7 @@ export async function middleware(req: NextRequest) {
       cookie: req.headers.get("cookie"),
     },
   };
-  const res = NextResponse.next();
+  
 
   const redirectUrl = req.nextUrl.clone();
 
